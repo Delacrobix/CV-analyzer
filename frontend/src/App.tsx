59e12cef9545f56b68afa94331a593faafe5f438
Jeffrey <jeffrey.rengifom@gmail.com>
2024-06-21 12:53:40 -0500

    route /analysis protected if there are no analysis

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,13 +8,23 @@ import {
 import BasePage from "./components/basePage";
 import Index from "./pages/index";
 import Analysis from "./pages/analysis";
+import { useGlobalStateVariables } from "./state/globalVariablesState";
 
 function App() {
+  const { analysisResult } = useGlobalStateVariables();
+
   return (
     <Router>
       <Routes>
         <Route path='/' element={<BasePage>{<Index />}</BasePage>} />
-        <Route path='/analysis' element={<BasePage>{<Analysis />}</BasePage>} />
+        <Route
+          path='/analysis'
+          element={
+            <BasePage>
+              {analysisResult ? <Analysis /> : <Navigate to='/' replace />}
+            </BasePage>
+          }
+        />
         <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
     </Router>
